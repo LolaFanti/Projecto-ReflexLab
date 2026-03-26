1bc9e3368a2ba771167ac2b842d0c1f995e5dcd3
henestrosa-clara <henestrosaclara@gmail.com>
2026-03-26 13:58:29 -0300

    docstring archivo procesamiento_datos

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -1,7 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 26 13:48:22 2026
+def  filtrar_por_participante (datos,id_participante) :
+    """
+    Selecciona los datos de un participante
 
-@author: lola_
-"""
+    Parameters
+    ----------
+    datos : list
+        Contiene los datos de los participantes.
+    id_participante : int
+        Del participante a filtrar.
 
+    Returns
+    datos_participante_filtrado : list
+        Con los datos ya filtrados del participante ingresado.
+
+    """
