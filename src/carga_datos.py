f03c3fa37ab9917957f867fe06bfc7c99964c242
henestrosa-clara <henestrosaclara@gmail.com>
2026-03-26 13:53:23 -0300

    docstrings archivo carga_datos

--- src/carga_datos.py
+++ src/carga_datos.py
@@ -1,7 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 26 13:43:31 2026
+def pasrsear_linea (linea):
+    """
+    Separa la línea en campos, convierte los valores al tipo correspondiente.
+    Parameters
+    ----------
+    linea : str
+        Informacion cruda del archivo.
 
-@author: lola_
-"""
+    Returns
+    dicc : diccionario
+        Diccionario con la informacion del archivo ordenado.
+    """
 
+def cargar_datos(ruta_archivo):
+    """
+    Carga datos desde un archivo y devuelve una lista de registros.
+    Parameters
+    ----------
+    ruta_archivo : str
+        Ruta del archivo a leer.
+
+    Returns
+    -------
+    lista_registros : list
+        Lista de registros parseados a partir del archivo.
+    """
+    
+    
