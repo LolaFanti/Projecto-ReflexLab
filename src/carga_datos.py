b43fda3e7f11d6eedfc4e841972ff2aab64a3443
omuller07 <omuller@udesa.edu.ar>
2026-04-16 14:31:50 -0300

    mover archivos

--- src/carga_datos.py
+++ src/carga_datos.py
@@ -44,8 +44,8 @@ def cargar_datos(ruta_archivo):
     try: 
         with open(ruta_archivo + ".csv", "r") as archivo: 
             lineas = archivo.readlines()
-    except:
-        raise FileNotFoundError ("Archivo no encontrado")
+    except Exception:
+        raise FileNotFoundError("Archivo no encontrado")
     
     lista= []
     
