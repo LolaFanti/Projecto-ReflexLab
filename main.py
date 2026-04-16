8f458d02f5af353ee191044d79c693e06b998a9d
henestrosa-clara <henestrosaclara@gmail.com>
2026-04-16 14:12:21 -0300

    Try except en el main

--- main.py
+++ main.py
@@ -9,19 +9,24 @@ from src.procesamiento_datos import filtrar_por_participante
 from src.metricas import calcular_tiempo_reaccion_promedio, calcular_tasa_error
 
 ruta_archivo = input("Ingresá el nombre del archivo sin .csv: ")
-datos = cargar_datos(ruta_archivo)
+try:
+    datos = cargar_datos(ruta_archivo)
+    
+except FileNotFoundError as e:
+    print(e)
+    
+else:
+    id_participante = input("Ingresá el id del participante: ")
+    datos_participante = filtrar_por_participante(datos, id_participante)
 
-id_participante = input("Ingresá el id del participante: ")
-datos_participante = filtrar_por_participante(datos, id_participante)
-
-promedio_tiempo = calcular_tiempo_reaccion_promedio(datos_participante)
-tasa_error = calcular_tasa_error(datos_participante)
-
-print("Resultados del análisis")
-print("Participante:", id_participante)
-print("Cantidad de registros:", len(datos_participante["datos"]))
-print("Tiempo de reacción promedio:", promedio_tiempo)
-print("Tasa de error:", tasa_error)
+    promedio_tiempo = calcular_tiempo_reaccion_promedio(datos_participante)
+    tasa_error = calcular_tasa_error(datos_participante)
+    
+    print("Resultados del análisis")
+    print("Participante:", id_participante)
+    print("Cantidad de registros:", len(datos_participante["datos"]))
+    print("Tiempo de reacción promedio:", promedio_tiempo)
+    print("Tasa de error:", tasa_error)
 
 
 
