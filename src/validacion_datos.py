2bb82dc070c0a4358bc60ee1322b7b023138c3d9
eiturriaga <eiturriaga@udesa.edu.ar>
2026-04-22 11:44:46 -0300

    Update validar positivo

--- src/validacion_datos.py
+++ src/validacion_datos.py
@@ -5,11 +5,26 @@ Created on Thu Mar 26 13:46:50 2026
 @author: lola_
 """
 
-def trial_entero_positivo(numero):
-     numero= int(numero)
-     if numero>0:
-         return numero 
-     else: 
-         raise ValueError("El numero del trial es negativo")
+def trial_positivo(numero):
+    """"
+  
+    Convierte el numero en un entero (int). 
+    Verifica que el numero sea positivo
+    ----------
+    numero: int
+        Numero a verificar que sea positivo.  
+
+
+    Returns
+    -------
+    Numero: int
+        Numero entero y positivo. 
+   
+    """
+    
+    if numero>0:
+        return numero 
+    else: 
+        raise ValueError("El numero del trial es negativo")
      
 
