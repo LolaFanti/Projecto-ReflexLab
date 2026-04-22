637ba7976df1663afb99cf5a06b2c8aafa0c0ece
eiturriaga <eiturriaga@udesa.edu.ar>
2026-04-22 11:35:23 -0300

    Trial entero positivo

--- src/validacion_datos.py
+++ src/validacion_datos.py
@@ -5,3 +5,11 @@ Created on Thu Mar 26 13:46:50 2026
 @author: lola_
 """
 
+def trial_entero_positivo(numero):
+     numero= int(numero)
+     if numero>0:
+         return numero 
+     else: 
+         raise ValueError("El numero del trial es negativo")
+     
+
